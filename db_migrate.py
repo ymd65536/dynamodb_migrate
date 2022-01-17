27d1a265812c1721cc466a19d61810a6a62e3cc1
ymd65536 <ymd65536@gmail.com>
2022-01-17 14:59:34 +0900

    initial commit

---
+++ db_migrate.py
@@ -0,0 +1,18 @@
+import json
+from score_model import Score
+
+if not Score.exists():
+  Score.create_table(read_capacity_units=1,
+                    write_capacity_units=1, wait=True)
+
+with open('flex_messages.json', 'r',encoding="utf-8") as f:
+    flex_messages = json.load(f)
+
+# スコア情報を挿入する
+Score(question_id='q1', question=json.dumps(flex_messages['q1']),
+      answer='3', score=10).save()
+Score(question_id='q2', question=json.dumps(flex_messages['q2']),
+      answer='1', score=20).save()
+Score(question_id='q3', question=json.dumps(flex_messages['q3']),
+      answer='2', score=20).save()
+print('Done!')
