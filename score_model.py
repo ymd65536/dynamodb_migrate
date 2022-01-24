f93a3b0b9cbece52598c1ca837eb744e0f02067f
Kento.Yamada <ymd65536@gmail.com>
2022-01-24 21:31:46 +0900

    スコアモデル

--- score_model.py
+++ score_model.py
@@ -1,16 +1,16 @@
 
+import os
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute,NumberAttribute
-
-endpoint_url = 'http://localhost:8000'
 
 class Score(Model):
   class Meta:
     table_name = 'Score'
     region = 'ap-northeast-1'
-    # aws_access_key_id = 'ACCESS_ID'
-    # aws_secret_access_key = 'ACCESS_KEY'
-    host = endpoint_url
+    read_capacity_units = 1
+    write_capacity_units = 1
+    aws_access_key_id = os.getenv('aws_access_key_id')
+    aws_secret_access_key = os.getenv('aws_secret_access_key')
 
   question_id = UnicodeAttribute(hash_key=True)
   question = UnicodeAttribute()
